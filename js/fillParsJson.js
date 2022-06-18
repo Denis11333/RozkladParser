20526bc83f5ffc2ff9f0ee3cd21f0bc2e1990fa2
Denis <77487610+Denis11333@users.noreply.github.com>
2022-06-18 11:12:45 +0300

    Update fillParsJson.js

--- js/fillParsJson.js
+++ js/fillParsJson.js
@@ -13,7 +13,7 @@ let globalObject = {
         }
         const MongoClient = require("mongodb").MongoClient
 
-        const url = "mongodb://127.0.0.1:27017/";
+        const url = "mongodb://localhost:27017/";
         const mongoClient = new MongoClient(url);
         mongoClient.connect(function (err, client) {
 
